cd0e89f0b1c003215bdfad798f66f08566ade0fc
timer z <127018624+ihohv@users.noreply.github.com>
2024-07-19 11:09:09 +0800

    Update Binaryprint.java

--- Binaryprint.java
+++ Binaryprint.java
@@ -1,12 +1,15 @@
-
 public class Binaryprint {
+
+	// 打印一个int类型的数字，32位进制的状态
+	// 左侧是高位，右侧是低位
 	public static void binaryPrint(int num) {
-		for(int i=31;i>=0;i--) {
-			System.out.print(((1<<i)&num) == 0?"0":"1");
+		for (int i = 31; i >= 0; i--) {
+			System.out.print(((1 << i) & num) == 0 ? "0" : "1");
 		}
 		System.out.println();
 	}
-	public static void main(String []args) {
+
+	public static void main(String[] args) {
 		int n = -8;
 		binaryPrint(n);
 	}
