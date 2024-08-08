cd16fe8789b4df22a92305a3e92df337c46f7af4
timer z <127018624+ihohv@users.noreply.github.com>
2024-08-08 18:25:50 +0800

    Add files via upload

--- MergeKSortedLists.java
+++ MergeKSortedLists.java
@@ -1,4 +1,35 @@
+import java.util.ArrayList;
+import java.util.PriorityQueue;
 
 public class MergeKSortedLists {
 
+	public static class ListNode {
+		public int val;
+		public ListNode next;
+	}
+
+	public static ListNode mergeKLists(ArrayList<ListNode> arr) {
+		PriorityQueue<ListNode> heap = new PriorityQueue<>((a, b) -> a.val - b.val);
+		for (ListNode h : arr) {
+			if (h != null) {
+				heap.add(h);
+			}
+		}
+		if (heap.isEmpty()) {
+			return null;
+		}
+		ListNode h = heap.poll();
+		ListNode pre = h;
+		if (pre.next != null) {
+			heap.add(pre.next);
+		}
+		while (!heap.isEmpty()) {
+			pre.next = heap.poll();
+			pre = pre.next;
+			if (pre.next != null) {
+				heap.add(pre.next);
+			}
+		}
+		return h;
+	}
 }
